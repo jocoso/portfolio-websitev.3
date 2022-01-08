0c644bd3eb856bfa3717462976ad8ce01f4fac75
jocoso <joshua.collado022@gmail.com>
2022-01-08 15:12:17 -0500

    Created the frame that will contain the about page.

---
+++ src/views/Frame.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+/*Contains all the components that remains regardless of the content */
+class Frame extends Component {
+    render() {
+        return (
+            <div>
+                {/*Navigation Bar*/}
+                {/*Content*/}
+                {/*Contact Info*/}
+            </div>
+        );
+    }
+}
+
+export default Frame;
