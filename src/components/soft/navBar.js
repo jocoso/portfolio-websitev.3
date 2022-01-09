5c14641672e262b5088255cb97ece7317b95e715
jocoso <joshua.collado022@gmail.com>
2022-01-09 08:32:41 -0500

    Added grid to make all look pretty

--- src/components/soft/navBar.js
+++ src/components/soft/navBar.js
@@ -4,7 +4,6 @@ export default function NavBar(props) {
     return(
         
         <ul id='nav-bar'>
-            <img src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetdrawings.com%2Fimages%2Fscary-spider-drawing-4.png&f=1&nofb=1' />
             [{props.data.map(item => {
                 return <li onClick={() => props.onClick(item.id)}>
                             {item.id}. {item.name} 
